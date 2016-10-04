a00a7c5fcf6128cef6a8ae07f11cc6206f3275c0
Samantha Monteiro <samanthakem@gmail.com>
2016-10-03 22:09:25 -0700

    doing some adjustments

--- popup.js
+++ popup.js
@@ -14,14 +14,17 @@ var openInNewTab = function(url, handle) {
   	win.focus();
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
 	var submit_btn = document.getElementById('submit')
 	submit_btn.addEventListener('click', function() {
 	var handle = document.getElementById('handle-input').value;
 
-	$("input[type=checkbox]:checked").each (function() {
+	if (handle.length != 0) {
+		$("input[type=checkbox]:checked").each (function() {
 		var social_ntw = $(this).val();
 			openInNewTab(social_ntws[social_ntw], handle);
-	});
+		});
+	}
 });
 }, false);
